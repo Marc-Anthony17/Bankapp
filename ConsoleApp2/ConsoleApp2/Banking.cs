8f6ddc830f2e006e12a863183f52db4a572d2b8b
Marc-Anthony17 <102612134+Marc-Anthony17@users.noreply.github.com>
2024-02-27 20:38:50 +0200

    Add attributes with corresponding getters and setters for enhanced functionality and data encapsulation.

---
+++ ConsoleApp2/ConsoleApp2/Banking.cs
@@ -0,0 +1,69 @@
+﻿namespace FirstApp
+{
+    using System;
+    internal class Banking
+    {
+        String cardNum;
+        int pin;
+        String firstName;
+        String lastName;
+        double balence;
+
+        public Banking(string cardNum, int pin, string firstName, string lastName, double balence)
+        {
+            this.cardNum = cardNum;
+            this.pin = pin;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.balence = balence;
+        }
+
+        public String getCardNum()
+        {
+            return cardNum;
+        }
+        public String getFirstName()
+        {
+            return firstName;
+        }
+        public String getLastName() { return lastName; }
+        public int getPin()
+        {
+            return pin;
+        }
+        public double getBalence() { return balence; }
+        public void setPin(int pin)
+        {
+            this.pin = pin;
+        }
+        public void setFirstName(String firstName)
+        {
+            this.firstName = firstName;
+        }
+        public void setName(String lastName)
+        {
+            this.lastName = lastName;
+        }
+        public void setCardNum(String cardNum)
+        {
+            this.cardNum = cardNum;
+        }
+        public void setBalence(double balence) { this.balence = balence; }
+
+        public static void Main(String[] args)
+        {
+            void displayInstructions () {
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Show Balence");
+                Console.WriteLine("4. Exit");
+
+
+            }
+           // void deposit(Banking currentCustomer) {
+                //try()
+           // }
+        }
+    }
+}
+
