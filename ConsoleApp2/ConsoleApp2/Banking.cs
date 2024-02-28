ed5fc3234c7043564f6c30b26f5b1dcf91f38eff
Marc-Anthony17 <102612134+Marc-Anthony17@users.noreply.github.com>
2024-02-28 15:07:18 +0200

    Completed the application, still open to improving it.

--- ConsoleApp2/ConsoleApp2/Banking.cs
+++ ConsoleApp2/ConsoleApp2/Banking.cs
@@ -1,69 +1,218 @@
-﻿namespace FirstApp
+﻿
+namespace FirstApp
 {
-    using System;
     internal class Banking
     {
-        String cardNum;
-        int pin;
-        String firstName;
-        String lastName;
-        double balence;
+        private string cardNum;
+        private int pin;
+        private string firstName;
+        private string lastName;
+        private double balance;
 
-        public Banking(string cardNum, int pin, string firstName, string lastName, double balence)
+        public Banking(string cardNum, int pin, string firstName, string lastName, double balance)
         {
             this.cardNum = cardNum;
             this.pin = pin;
             this.firstName = firstName;
             this.lastName = lastName;
-            this.balence = balence;
+            this.balance = balance;
         }
 
-        public String getCardNum()
+        public string GetCardNum()
         {
             return cardNum;
         }
-        public String getFirstName()
+
+        public string GetFirstName()
         {
             return firstName;
         }
-        public String getLastName() { return lastName; }
-        public int getPin()
+
+        public string GetLastName()
+        {
+            return lastName;
+        }
+
+        public int GetPin()
         {
             return pin;
         }
-        public double getBalence() { return balence; }
-        public void setPin(int pin)
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void SetPin(int pin)
         {
             this.pin = pin;
         }
-        public void setFirstName(String firstName)
+
+        public void SetFirstName(string firstName)
         {
             this.firstName = firstName;
         }
-        public void setName(String lastName)
+
+        public void SetLastName(string lastName)
         {
             this.lastName = lastName;
         }
-        public void setCardNum(String cardNum)
+
+        public void SetCardNum(string cardNum)
         {
             this.cardNum = cardNum;
         }
-        public void setBalence(double balence) { this.balence = balence; }
 
-        public static void Main(String[] args)
+        public void SetBalance(double balance)
         {
-            void displayInstructions () {
+            this.balance = balance;
+        }
+
+        public static void Main(string[] args)
+        {
+            void DisplayInstructions()
+            {
                 Console.WriteLine("1. Deposit");
                 Console.WriteLine("2. Withdraw");
-                Console.WriteLine("3. Show Balence");
+                Console.WriteLine("3. Show Balance");
                 Console.WriteLine("4. Exit");
-
-
             }
-           // void deposit(Banking currentCustomer) {
-                //try()
-           // }
+
+            void DepositToBank(Banking currentCustomer)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("How much would you like to deposit: ");
+                        string deposit = Console.ReadLine();
+                        double amountValue = Convert.ToDouble(deposit);
+                        currentCustomer.SetBalance(currentCustomer.GetBalance() + amountValue);
+                        Console.WriteLine("Thank you for your deposit. your current Value is: " + currentCustomer.GetBalance());
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                    }
+                }
+            }
+
+            void Withdraw(Banking currentCustomer)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("How much would you like to withdraw: ");
+                        string amount = Console.ReadLine();
+                        double amountValue = Convert.ToDouble(amount);
+
+                        if (amountValue > currentCustomer.GetBalance())
+                        {
+                            Console.WriteLine("The specified amount is greater than the amount you have.");
+                            continue;
+                        }
+
+                        currentCustomer.SetBalance(currentCustomer.GetBalance() - amountValue);
+                        Console.WriteLine("Withdrawal was successful. your current amount is: " + currentCustomer.GetBalance());
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                    }
+                }
+            }
+
+            void ViewBalance(Banking currentCustomer)
+            {
+                Console.WriteLine($"Your current balance: {currentCustomer.GetBalance()}");
+            }
+
+            List<Banking> customers = new List<Banking>
+            {
+                new Banking("0000000000000", 1234, "Adam", "Blake", 87000.30),
+                new Banking("0000000000002", 2334, "Eve", "Ronald", 80000.50)
+            };
+            Banking validUser;
+            string userCardNumber = "";
+
+            Console.WriteLine("Welcome to the best bank in the east.");
+
+            while (true)
+            {
+                Console.WriteLine("Please enter your account number: ");
+                userCardNumber = Console.ReadLine();
+
+                validUser = customers.FirstOrDefault(a => a.GetCardNum() == userCardNumber);
+
+                if (validUser != null)
+                {
+                    Console.WriteLine($"Welcome, {validUser.GetFirstName()}, to the best bank ever");
+                    break;
+                }
+
+                Console.WriteLine("Card not recognized. Please try again.");
+            }
+
+            Console.WriteLine("Please enter pin: ");
+            int pin = 0;
+
+            while (true)
+            {
+                try
+                {
+                    pin = int.Parse(Console.ReadLine());
+
+                    if (validUser.GetPin() != pin)
+                    {
+                        Console.WriteLine("Pin incorrect. Please try again.");
+                        continue;
+                    }
+
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid pin.");
+                }
+            }
+
+            int option = 0;
+
+            do
+            {
+                DisplayInstructions();
+
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+                            DepositToBank(validUser);
+                            break;
+                        case 2:
+                            Withdraw(validUser);
+                            break;
+                        case 3:
+                            ViewBalance(validUser);
+                            break;
+                        case 4:
+                            Console.WriteLine("Thank you. Have a nice day.");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option. Please try again.");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid option.");
+                }
+            } while (option != 4);
         }
     }
 }
-
--- ConsoleApp2/ConsoleApp2/obj/Debug/net8.0/FirstApp.AssemblyInfo.cs
+++ ConsoleApp2/ConsoleApp2/obj/Debug/net8.0/FirstApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6ddc830f2e006e12a863183f52db4a572d2b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
